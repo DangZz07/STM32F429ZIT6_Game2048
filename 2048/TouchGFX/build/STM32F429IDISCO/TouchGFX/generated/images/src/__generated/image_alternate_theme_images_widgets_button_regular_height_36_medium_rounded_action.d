b07cd162ea227c76535f30cb46f50bc5d1c77ebd
Trần Mạnh Hùng <166214532+hosobaomat@users.noreply.github.com>
2025-07-02 11:16:11 +0700

    sua loi man supermerging

--- 2048/TouchGFX/build/STM32F429IDISCO/TouchGFX/generated/images/src/__generated/image_alternate_theme_images_widgets_button_regular_height_36_medium_rounded_action.d
+++
@@ -1,5 +0,0 @@
-TouchGFX/build/STM32F429IDISCO/TouchGFX/generated/images/src/__generated/image_alternate_theme_images_widgets_button_regular_height_36_medium_rounded_action.o: \
- TouchGFX/generated/images/src/__generated/image_alternate_theme_images_widgets_button_regular_height_36_medium_rounded_action.cpp \
- Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp
-
-Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp:
